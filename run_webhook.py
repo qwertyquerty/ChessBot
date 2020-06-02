da1c147aab23fee9f3fe0d1318a5345ef8833280
qwerty <qwertytrogi@gmail.com>
2020-06-02 18:41:53 -0500

    pakageify the webhook

--- run_webhook.py
+++ run_webhook.py
@@ -1,22 +1,4 @@
-from flask import Flask, abort, request
-
 from chessbot.config import *
-from chessbot import db
-
-app = Flask(__name__)
-
-@app.route("/api/vote", methods = ['POST'])
-def page_api_vote():
-
-	if request.headers["Authorization"] != WEBHOOK_TOKEN:
-		return abort(401)
-
-	user = db.User.from_user_id(request.json["user"])
-
-	if not user:
-		return abort(400)
-	
-	user.inc("votes", 1)
-	return abort(200)
+from chessbot.webhook import app
 
 app.run("localhost", port=WEBHOOK_PORT)
