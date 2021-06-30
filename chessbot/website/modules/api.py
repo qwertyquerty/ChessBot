62a0da0b0c8fb557bee049ccf027faad25e7fdc3
qwerty <qwertytrogi@gmail.com>
2021-06-30 02:28:44 -0500

    try again

--- chessbot/website/modules/api.py
+++ chessbot/website/modules/api.py
@@ -11,8 +11,8 @@ def page_api_vote():
 	if request.headers["Authorization"] != WEBHOOK_TOKEN:
 		return abort(401)
 	
-	print(request.args)
-	uid = int(request.json()["user"])
+	print(request.form)
+	uid = int(request.json["user"])
 
 	user = db.User.from_user_id(uid)
 
